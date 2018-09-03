4202c6b3fb36b3dd0ac8247b6fef6960b650c441
Kyle <kkumar.75511@farnborough.ac.uk>
2018-09-03 01:23:37 +0100

    Commit #2

--- js/main.js
+++ js/main.js
@@ -41,23 +41,23 @@ $(function() {
 // Smooth scrolling
 $('.cf a').on('click', function(event) {
 
-    if (this.hash !== '') {
-        
-      event.preventDefault();
-  
-      const hash = this.hash;
-  
-      $('html, body').animate(
-        {
-          scrollTop: $(hash).offset().top
-        },
-        800,
-        function() {
-          window.location.hash = hash;
-        }
-        
-      );
+  if (this.hash !== '') {
 
-    }
+    event.preventDefault();
 
-  });
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+
+    );
+
+  }
+
+});
